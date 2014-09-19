0c7ecdf700d93e4485959cc37b179bd8567f88de
Christian Parpart <trapni@gmail.com>
2014-09-19 23:40:00 +0200

    initial logging facility

---
+++ include/xzero/logging/LogSource.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <xzero/Api.h>
+#include <string>
+
+namespace xzero {
+
+/**
+ * A logging source.
+ *
+ * Creates a log message such as "[$ClassName] $LogMessage"
+ *
+ * Your class that needs logging creates a static member of LogSource
+ * for generating logging messages.
+ */
+class XZERO_API LogSource {
+ public:
+  explicit LogSource(const std::string& className);
+  ~LogSource();
+
+  void debug(const char* fmt, ...);
+  void info(const char* fmt, ...);
+  void warn(const char* fmt, ...);
+  void error(const char* fmt, ...);
+
+  void enable();
+  bool isEnabled() const noexcept;
+  void disable();
+
+  const std::string& className() const noexcept { return className_; }
+
+ private:
+  std::string className_;
+  bool enabled_;
+};
+
+}  // namespace xzero
---
+++ include/xzero/logging/LogTarget.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <xzero/Api.h>
+#include <string>
+#include <unordered_map>
+
+namespace xzero {
+
+/**
+ * Log Target Interface.
+ *
+ * Logging target implementations (such as a console logger or syslog logger)
+ * must implement this interface.
+ */
+class XZERO_API LogTarget {
+ public:
+  virtual ~LogTarget() {}
+
+  virtual void debug(const std::string& msg) = 0;
+  virtual void info(const std::string& msg) = 0;
+  virtual void warn(const std::string& msg) = 0;
+  virtual void error(const std::string& msg) = 0;
+
+  static LogTarget* console(); // standard console logger
+  static LogTarget* syslog();  // standard syslog logger
+};
+
+}  // namespace xzero
