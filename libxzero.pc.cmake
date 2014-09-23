227d778e4eea870fe09c837606f8ae96089e6bd1
Christian Parpart <trapni@gmail.com>
2014-09-23 21:47:33 +0200

    pkg-config fixes

--- libxzero.pc.cmake
+++ libxzero.pc.cmake
@@ -5,5 +5,5 @@ Description: Xzero HTTP library
 Version: @LIBXZERO_VERSION@
 # Requires:
 # Conflicts:
-Libs: -lxzero
-# Cflags:
+Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lxzero -lev -lpthread -pthread @POSIX_RT_LIBS@
+Cflags: -I@CMAKE_INSTALL_PREFIX@/include -pthread @CFLAGS_RDYNAMIC@ @CXXFLAGS@
