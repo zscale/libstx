9a267efce3cab4cfc6fd3a0193ddaad31344ae83
Christian Parpart <trapni@gmail.com>
2014-09-25 23:19:15 +0200

    HTTP's MockTransport is a little bit more complete now

--- include/xzero/http/mock/MockTransport.h
+++ include/xzero/http/mock/MockTransport.h
@@ -13,16 +13,61 @@ class Executor;
 
 /**
  * Mock HTTP Transport, used to create mock requests.
+ *
+ * This HTTP transport implementation is not using byte streams to communicate
+ * but high level data structures to create requests and provides
+ * access to high level data structures to read out the response message.
+ *
+ * The Executor service is only used for completion handlers.
+ *
+ * The actual HTTP request message handler is custom passed to the constructor.
+ *
+ * @note This object is not thread safe.
+ *
+ * @see HttpResponseInfo
+ * @see HttpHandler
+ * @see Executor
  */
 class XZERO_API MockTransport : public HttpTransport {
  public:
+  /**
+   * Initializes the mock transport object.
+   *
+   * @param executor the executor service used for completion handlers.
+   * @param handler the handler to run for incoming HTTP request messages.
+   */
   explicit MockTransport(Executor* executor, const HttpHandler& handler);
   ~MockTransport();
 
+  /**
+   * Runs given HTTP request message.
+   *
+   * @param version HTTP message version, such as @c HttpVersion::VERSION_1_1.
+   * @param method HTTP request method, such as @c "GET".
+   * @param entity HTTP request entity, such as @c "/index.html".
+   * @param headers HTTP request headers.
+   * @param body HTTP request body.
+   *
+   * @see responseInfo()
+   * @see responseBody()
+   */
   void run(HttpVersion version, const std::string& method,
            const std::string& entity, const HeaderFieldList& headers,
            const std::string& body);
 
+  /** Retrieves the response message status line and headers. */
+  const HttpResponseInfo& responseInfo() const noexcept { return responseInfo_; }
+
+  /** Retrieves the response message body. */
+  const Buffer& responseBody() const noexcept { return responseBody_; }
+
+  /** Tests whether last this transport was aborted in last request handling. */
+  bool isAborted() const noexcept { return isAborted_; }
+
+  /** Tests whether last message. */
+  bool isCompleted() const noexcept { return isCompleted_; }
+
+ private:
   // HttpTransport overrides
   void abort() override;
   void completed() override;
@@ -38,13 +83,12 @@ class XZERO_API MockTransport : public HttpTransport {
   void onFlushable() override;
   bool onReadTimeout() override;
 
-  const HttpResponseInfo& responseInfo() const noexcept { return responseInfo_; }
-  const Buffer& responseBody() const noexcept { return responseBody_; }
-
  private:
   Executor* executor_;
   HttpHandler handler_;
 
+  bool isAborted_;
+  bool isCompleted_;
   std::unique_ptr<HttpChannel> channel_;
   HttpResponseInfo responseInfo_;
   Buffer responseBody_;
