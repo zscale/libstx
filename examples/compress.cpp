99160e8c12ead8c7473a935f3dbdb7db270aba76
Christian Parpart <trapni@gmail.com>
2014-11-03 16:21:45 +0100

    adds a gzip compression filter example

---
+++ examples/compress.cpp
@@ -0,0 +1,32 @@
+#include <xzero/io/GzipFilter.h>
+#include <xzero/Buffer.h>
+#include <unistd.h>
+#include <stdio.h>
+
+using namespace xzero;
+
+int main(int argc, const char* argv[]) {
+  GzipFilter gzip(9);
+  Buffer input(4096);
+  Buffer output(4096);
+
+  while (!feof(stdin)) {
+    ssize_t n = fread(input.data(), 1, input.capacity(), stdin);
+    if (n < 0) {
+      perror("fread");
+      return 1;
+    }
+    input.resize(n);
+    fprintf(stderr, "read %zi bytes\n", n);
+    gzip.filter(input, &output, false);
+    fprintf(stderr, "compressed to %zu bytes\n", output.size());
+    fwrite(output.data(), 1, output.size(), stdout);
+    output.clear();
+  }
+  gzip.filter(BufferRef(), &output, true);
+  fprintf(stderr, "compressed to %zu bytes (finalize)\n", output.size());
+  fwrite(output.data(), output.size(), 1, stdout);
+  fflush(stdout);
+
+  return 0;
+}
