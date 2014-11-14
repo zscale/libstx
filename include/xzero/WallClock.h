5d547f1ac3ad4b2fdba01c52ffcb6e994b482c11
Christian Parpart <trapni@gmail.com>
2014-11-15 00:25:10 +0100

    WallClock: adds missing header guard

--- include/xzero/WallClock.h
+++ include/xzero/WallClock.h
@@ -5,6 +5,8 @@
 // file except in compliance with the License. You may obtain a copy of
 // the License at: http://opensource.org/licenses/MIT
 
+#pragma once
+
 #include <xzero/Api.h>
 #include <xzero/DateTime.h>
 
