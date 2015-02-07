368597c5a289706b53f34edef8a30e425ea0d2d4
Laura Schlimmer <laura@etat-gray.com>
2015-02-07 16:49:52 +0000

    moved logic to fnord.js

--- fnord-cockpit/fn-cockpit-metric-explorer.php
+++ fnord-cockpit/fn-cockpit-metric-explorer.php
@@ -19,8 +19,8 @@ function fn_render_metric_explorer($rpc_url) {
 </div>
 
 <script type='text/javascript'>
-  var baseUrl = "<?= $rpc_url ?>";
-
+  //var baseUrl = "<?= $rpc_url ?>";
+  var baseUrl = "http://s01.nue01.production.fnrd.net:7009/metrics/"
   var views = {
     "metrics" : "<fn-metric-explorer-list></fn-metric-explorer-list>",
     "metric" : "<fn-metric-explorer-preview></fn-metric-explorer-preview>",
@@ -34,9 +34,8 @@ function fn_render_metric_explorer($rpc_url) {
   };
 
   var default_route = "metrics";
-
   function openUrl(raw_url, push_state) {
-    var url = parseUrlQueryString(raw_url);
+    var url = Fnord.parseUrlQueryString(raw_url);
     var query_params = url["query_params"];
 
     var view_name = routes[url["path"]];
@@ -61,135 +60,6 @@ function fn_render_metric_explorer($rpc_url) {
     viewport.appendChild(view);
 
   };
-
-
-  function parseUrlQueryString(qstr) {
-    if (qstr == null) {return;}
-    var path;
-    var query_params = {};
-
-    if (qstr.indexOf("?") >= 0) {
-      path = qstr.substr(0, qstr.indexOf("?"));
-      path = path.replace("#", "");
-
-      var params_str = qstr.substr(qstr.indexOf("?") + 1);
-      var raw_params = params_str.split('&');
-
-      /* set first param which defines view's view (metric, search ...) */
-      var param = raw_params[0].split('=');
-      query_params.innerView = decodeURIComponent(param[0]);
-      query_params.innerViewValue = decodeURIComponent(param[1]);
-
-      for (var i = 1; i < raw_params.length; i++) {
-        var param = (raw_params[i].split('=') != "undefined") ? 
-          raw_params[i].split('=') : "";
-        if (param[0] != "undefined") {
-          query_params[decodeURIComponent(param[0])] =
-             (param[1] != "undefined") ? 
-             decodeURIComponent(param[1]) : "";
-        }
-      }
-
-    } else {
-      path = qstr != "undefined" ? qstr : "";
-    }
-
-    return {
-      "path": path,
-      "query_params": query_params
-    }
-  };
-
-  function isMetricParam(key) {
-    var metricParamKeys = ["aggr_fn", "aggr_window", "aggr_step", "scale", "group_by"];
-    return (metricParamKeys.indexOf(key) > -1);
-  }
-
-  function isGenericParam(key) {
-    //params that can be changed
-    var genericParamKeys = ["from", "until", "logarithmic", "inverted", "metrics", "format"];
-    return (genericParamKeys.indexOf(key) > -1);
-  }
-
-  function parseMetricQueryUrl(qstr) {
-    if (qstr == null) {return;}
-    var path;
-    var params = {};
-    var metricCollector = {};
-    var metricIndex = 0;
-
-    if (qstr.indexOf("?") >= 0) {
-      path = qstr.substr(0, qstr.indexOf("?"));
-      path = path.replace("#", "");
-
-      var params_str = qstr.substr(qstr.indexOf("?") + 1);
-      var raw_params = params_str.split('&');
-
-      /*set main metric */
-      var metric = raw_params[0].split('=');
-      if (metric[0] != "metric") {return;}
-
-      metricCollector.name = decodeURIComponent(metric[1]);
-
-      for (var i = 1; i < raw_params.length; i++) {
-        var param = raw_params[i].split("=");
-        if (!param[0]) {continue;}
-
-        var key = decodeURIComponent(param[0]);
-        var value = decodeURIComponent(param[1]);
-
-        if (isMetricParam(key)) {
-          metricCollector[key] = value;
-        } else if (key == "metric") {
-          //new submetric
-          params["metric" + metricIndex] = metricCollector;
-          metricIndex++;
-          mainMetric = false;
-          metricCollector = {};
-          metricCollector.name = value;
-        } else if (isGenericParam(key)) {
-          params[key] = value;
-        }
-      }
-
-      params["metric" + metricIndex] = metricCollector;
-      params.metrics = metricIndex + 1;
-    }
-
-    return params;
-  };
-
-  function setUrlFromQueryString(hash, query_params, push_state) {
-    if (hash === undefined || hash === "undefined") {
-      window.location.hash = "";
-      return;
-    }
-    var path = "#" + hash;
-
-    if ("innerView" in query_params && query_params.innerView != undefined) {
-      path += "?" + encodeURIComponent(query_params.innerView) + "=";
-      path +=
-        encodeURIComponent(query_params.innerViewValue);
-
-      for (var param in query_params) {
-        if (param != "innerView" && 
-            param != "innerViewValue" &&
-            query_params[param] != undefined &&
-            query_params[param].length > 0) {
-
-          path += 
-            "&" + encodeURIComponent(param) +
-            "=" + encodeURIComponent(query_params[param]);
-        }
-      }
-    }
-
-    if (push_state) {
-      window.history.pushState({url:path}, "#", path);
-    }
-    window.location.hash = path;
-    return path;
-  }
 
   function setUrlHash(url_hash) {
     window.history.pushState({url: url_hash}, "#", url_hash);
