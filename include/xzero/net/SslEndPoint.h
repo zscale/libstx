a843bb3d1ddc855474db08f16a88d20a9b88b510
Christian Parpart <trapni@gmail.com>
2015-02-12 23:38:53 +0100

    snap

--- include/xzero/net/SslEndPoint.h
+++ include/xzero/net/SslEndPoint.h
@@ -7,23 +7,55 @@
 #pragma once
 
 #include <xzero/net/EndPoint.h>
+#include <xzero/IdleTimeout.h>
+#include <openssl/ssl.h>
 
 namespace xzero {
 
 class SslConnector;
+class Scheduler;
 
 class SslEndPoint : public EndPoint {
  public:
-  SslEndPoint(SslConnector* connector, ...);
+  SslEndPoint(int socket, SslConnector* connector, Scheduler* scheduler);
   ~SslEndPoint();
+
+  int handle() const noexcept { return handle_; }
 
   bool isOpen() const override;
   void close() override;
+
+  /**
+   * Reads from remote endpoint and fills given buffer with it.
+   */
   size_t fill(Buffer* sink) override;
+
+  /**
+   * Appends given buffer into the pending buffer vector and attempts to flush.
+   */
   size_t flush(const BufferRef& source) override;
   size_t flush(int fd, off_t offset, size_t size) override;
+
+  /**
+   * Ensures that the SSL socket is ready for receiving data.
+   *
+   * Any pending data in the fillBuffer will preceed.
+   *
+   * This might internally cause write <b>and</b> read operations
+   * through the SSL layer.
+   */
   void wantFill() override;
+
+  /** Ensures that the SSL socket is ready for more flush operations.
+   *
+   * Any pending data in the flushBuffer will be sent before
+   * @c connection()->onFlushable() will be invoked.
+   *
+   * This might internally cause write <b>and</b> read operations
+   * through the SSL layer.
+   */
   void wantFlush() override;
+
   TimeSpan idleTimeout() override;
   void setIdleTimeout(TimeSpan timeout) override;
   bool isBlocking() const override;
@@ -33,7 +65,22 @@ class SslEndPoint : public EndPoint {
   std::string toString() const override;
 
  private:
-  void* bio_;
+  void fillable();
+  void flushable();
+
+  void onHandshake();
+  void onRead();
+  void onWrite();
+  void onShutdown();
+
+ private:
+  int handle_;
+  SslConnector* connector_;
+  Scheduler* scheduler_;
+  IdleTimeout idleTimeout_;
+  SSL* ssl_;
+  Buffer readBuffer_;
+  Buffer writeBuffer_;
 };
 
 } // namespace xzero
