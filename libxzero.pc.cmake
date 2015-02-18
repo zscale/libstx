291b661045835dd82ac796d8c6422af10aaa5a40
Christian Parpart <trapni@gmail.com>
2015-02-18 23:19:04 +0100

    updates

--- libxzero.pc.cmake
+++ libxzero.pc.cmake
@@ -1,9 +1,9 @@
 # libxzero.pc.cmake
 
 Name: libxzero
-Description: Xzero HTTP library
-Version: @LIBXZERO_VERSION@
+Description: Xzero Base library
+Version: @XZERO_VERSION@
 # Requires:
 # Conflicts:
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lxzero -lev -lpthread -pthread @POSIX_RT_LIBS@
+Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lxzero-base -lev -lpthread -pthread @POSIX_RT_LIBS@
 Cflags: -I@CMAKE_INSTALL_PREFIX@/include -pthread @CFLAGS_RDYNAMIC@ @CXXFLAGS@
