93a650f3f0aff3e781a2f9063152290ceabaeb54
Christian Parpart <trapni@gmail.com>
2015-02-19 23:17:58 +0100

    cleanup

--- libxzero.pc.cmake
+++
@@ -1,9 +0,0 @@
-# libxzero.pc.cmake
-
-Name: libxzero
-Description: Xzero Base library
-Version: @XZERO_VERSION@
-# Requires:
-# Conflicts:
-Libs: -L@CMAKE_INSTALL_PREFIX@/lib -lxzero-base -lev -lpthread -pthread @POSIX_RT_LIBS@
-Cflags: -I@CMAKE_INSTALL_PREFIX@/include -pthread @CFLAGS_RDYNAMIC@ @CXXFLAGS@
