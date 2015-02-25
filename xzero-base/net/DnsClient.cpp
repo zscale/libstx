1fa89686d7fe8f04a88853f0607b49d922296124
Christian Parpart <trapni@gmail.com>
2015-02-25 23:43:43 +0100

    DnsClient: support filtering result by IP version

--- xzero-base/net/DnsClient.cpp
+++ xzero-base/net/DnsClient.cpp
@@ -6,67 +6,73 @@
 
 namespace xzero {
 
-// TODO: prefer getnameinfo as gethostbyname() is not IPv6-friendly
-
-std::vector<IPAddress> DnsClient::resolveAll(const std::string& name) {
+std::vector<IPAddress> DnsClient::resolveAll(const std::string& name, int ipv) {
   std::lock_guard<std::mutex> _lk(cacheMutex_);
   auto i = cache_.find(name);
   if (i != cache_.end())
-    return i->second;
+    return filter(i->second, ipv);
 
-  hostent* he = gethostbyname(name.c_str());
-  if (he == nullptr)
-    throw RUNTIME_ERROR(hstrerror(h_errno));
+  addrinfo* res = nullptr;
+  addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_flags = AI_NUMERICSERV;
+  hints.ai_family = AF_UNSPEC; // any
+  hints.ai_socktype = SOCK_STREAM; // any, actually
+
+  int rc = getaddrinfo(name.c_str(), nullptr, &hints, &res);
+  if (rc != 0)
+    throw RUNTIME_ERROR(gai_strerror(rc));
 
   std::vector<IPAddress> list;
-  for (int i = 0; he->h_addr_list[i]; ++i) {
-    switch (he->h_addrtype) {
+
+  for (addrinfo* ri = res; ri != nullptr; ri = ri->ai_next) {
+    switch (ri->ai_family) {
       case AF_INET:
-        list.push_back(IPAddress((in_addr*) he->h_addr_list[i]));
+        list.push_back(IPAddress((sockaddr_in*) ri->ai_addr));
         break;
       case AF_INET6:
-        list.push_back(IPAddress((in6_addr*) he->h_addr_list[i]));
+        list.push_back(IPAddress((sockaddr_in6*) ri->ai_addr));
         break;
       default:
         break;
     }
   }
+  return filter(cache_[name] = list, ipv);
+}
 
-  return cache_[name] = list;
+std::vector<IPAddress> DnsClient::resolveAll(const std::string& name) {
+  return resolveAll(name, IPAddress::V4);
+}
+
+IPAddress DnsClient::resolve(const std::string& name, int ipv) {
+  // TODO: optimize for speed
+  {
+    std::lock_guard<std::mutex> _lk(cacheMutex_);
+    auto i = cache_.find(name);
+    if (i != cache_.end())
+      return filter(i->second, ipv)[0];
+  }
+  return resolveAll(name, ipv)[0];
 }
 
 IPAddress DnsClient::resolve(const std::string& name) {
-  std::lock_guard<std::mutex> _lk(cacheMutex_);
-  auto i = cache_.find(name);
-  if (i != cache_.end())
-    return i->second[0];
-
-  hostent* he = gethostbyname(name.c_str());
-  if (he == nullptr)
-    throw RUNTIME_ERROR(hstrerror(h_errno));
-
-  std::vector<IPAddress> list;
-  for (int i = 0; he->h_addr_list[i]; ++i) {
-    switch (he->h_addrtype) {
-      case AF_INET:
-        list.push_back(IPAddress((in_addr*) he->h_addr_list[i]));
-        break;
-      case AF_INET6:
-        list.push_back(IPAddress((in6_addr*) he->h_addr_list[i]));
-        break;
-      default:
-        break;
-    }
-  }
-
-  cache_[name] = list;
-
-  return list[0];
+  return resolve(name, IPAddress::V4);
 }
 
 void DnsClient::clearCache() {
   std::lock_guard<std::mutex> _lk(cacheMutex_);
   cache_.clear();
+}
+
+std::vector<IPAddress> DnsClient::filter(
+    const std::vector<IPAddress>& ips, int ipv) {
+  std::vector<IPAddress> result;
+
+  for (const IPAddress& ip: ips)
+    if (ip.family() == ipv)
+      result.push_back(ip);
+
+  return result;
 }
 
 }  // namespace xzero
