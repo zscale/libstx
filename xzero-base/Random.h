2003d50e45d5f47687a6e03fba5e32d23e1cc84c
Christian Parpart <trapni@gmail.com>
2015-02-25 08:47:28 +0100

    Random: new API for generating random values

---
+++ xzero-base/Random.h
@@ -0,0 +1,59 @@
+// This file is part of the "x0" project, http://xzero.io/
+//   (c) 2011-2014 Paul Asmuth <paulasmuth@gmail.com>
+//   (c) 2009-2014 Christian Parpart <trapni@gmail.com>
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <xzero-base/Api.h>
+#include <xzero-base/RuntimeError.h>
+#include <string>
+#include <random>
+
+namespace xzero {
+
+/**
+ * Random Value Generator.
+ */
+class XZERO_API Random {
+ public:
+  Random();
+
+  /**
+   * Return 64 bits of random data
+   */
+  uint64_t random64();
+
+  /**
+   * Return 64 bits of random data as hex chars
+   */
+  std::string hex64();
+
+  /**
+   * Return 128 bits of random data as hex chars
+   */
+  std::string hex128();
+
+  /**
+   * Return 256 bits of random data as hex chars
+   */
+  std::string hex256();
+
+  /**
+   * Return 512 bits of random data as hex chars
+   */
+  std::string hex512();
+
+  /**
+   * Create a new random alphanumeric string
+   */
+  std::string alphanumericString(int nchars);
+
+ protected:
+  std::mt19937_64 prng_;
+};
+
+}  // namespace xzero
