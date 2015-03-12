ca27125a5cc3f468d71242dd9a090bb00164d42a
Paul Asmuth <paul@paulasmuth.com>
2015-03-12 20:04:58 +0100

    fnord::fts::QueryAnalyzer stub

---
+++ fnord-fts/StopwordDictionary.cc
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "fnord-fts/StopwordDictionary.h"
+
+namespace fnord {
+namespace fts {
+
+StopwordDictionary::StopwordDictionary() {}
+
+//const Set<String> stopwordsFor(Language lang) const;
+
+} // namespace fts
+} // namespace fnord
