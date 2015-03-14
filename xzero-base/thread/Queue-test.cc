4a316113af42083369b3594c149cab9cf2835881
Christian Parpart <trapni@gmail.com>
2015-03-14 01:41:10 +0100

    Queue: new API

---
+++ xzero-base/thread/Queue-test.cc
@@ -0,0 +1,36 @@
+// This file is part of the "libxzero" project, http://xzero.io/
+//   (c) 2009-2015 Christian Parpart <trapni@gmail.com>
+//   (c) 2014-2015 Paul Asmuth
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include <gtest/gtest.h>
+#include <xzero-base/thread/Queue.h>
+
+using namespace xzero;
+
+TEST(Queue, poll) {
+  thread::Queue<int> queue;
+  Option<int> o = queue.poll();
+  ASSERT_TRUE(o.isNone());
+
+  queue.insert(42);
+  o = queue.poll();
+  ASSERT_TRUE(o.isSome());
+  ASSERT_EQ(42, o.get());
+}
+
+TEST(Queue, pop) {
+  thread::Queue<int> queue;
+  queue.insert(42);
+
+  // TODO: make sure this was kinda instant
+  Option<int> o = queue.pop();
+  ASSERT_TRUE(o.isSome());
+  ASSERT_EQ(42, o.get());
+
+  // TODO: queue.pop() and have a bg thread inserting something 1s later.
+  // We expect pop to complete after 1s.
+}
---
+++ xzero-base/thread/Queue.h
@@ -0,0 +1,71 @@
+// This file is part of the "libxzero" project, http://xzero.io/
+//   (c) 2009-2015 Christian Parpart <trapni@gmail.com>
+//   (c) 2014-2015 Paul Asmuth
+//
+// Licensed under the MIT License (the "License"); you may not use this
+// file except in compliance with the License. You may obtain a copy of
+// the License at: http://opensource.org/licenses/MIT
+
+#include <xzero-base/Option.h>
+#include <condition_variable>
+#include <functional>
+#include <atomic>
+#include <deque>
+
+namespace xzero {
+namespace thread {
+
+/**
+ * A queue is threadsafe
+ */
+template <typename T>
+class Queue {
+public:
+  void insert(const T& job);
+  T pop();
+  Option<T> poll();
+
+protected:
+  std::deque<T> queue_;
+  std::mutex mutex_;
+  std::condition_variable wakeup_;
+};
+
+// {{{ inlines
+template <typename T>
+void Queue<T>::insert(const T& job) {
+  std::unique_lock<std::mutex> lk(mutex_);
+  queue_.emplace_back(job);
+  lk.unlock();
+  wakeup_.notify_one();
+}
+
+template <typename T>
+T Queue<T>::pop() {
+  std::unique_lock<std::mutex> lk(mutex_);
+
+  while (queue_.size() == 0) {
+    wakeup_.wait(lk);
+  }
+
+  auto job = queue_.front();
+  queue_.pop_front();
+  return job;
+}
+
+template <typename T>
+Option<T> Queue<T>::poll() {
+  std::unique_lock<std::mutex> lk(mutex_);
+
+  if (queue_.size() == 0) {
+    return None();
+  } else {
+    auto job = Some(queue_.front());
+    queue_.pop_front();
+    return job;
+  }
+}
+// }}}
+
+} // namespace thread
+} // namespace xzero
