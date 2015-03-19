bf8d040c260ec44c4756e2021a6b88a8ba72d280
Paul Asmuth <paul@paulasmuth.com>
2015-03-19 22:52:50 +0100

    set term buffer...

--- fnord-fts/analysis/TokenStream.cc
+++ fnord-fts/analysis/TokenStream.cc
@@ -1,9 +1,13 @@
-/////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2009-2014 Alan Wright. All rights reserved.
-// Distributable under the terms of either the Apache License (Version 2.0)
-// or the GNU Lesser General Public License.
-/////////////////////////////////////////////////////////////////////////////
-
+/*
+ * This file is part of the "libfnord" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include "fnord-base/stringutil.h"
 #include "fnord-fts/fts.h"
 #include "fnord-fts/analysis/TokenStream.h"
 #include "fnord-fts/analysis/tokenattributes/TermAttribute.h"
@@ -46,15 +50,13 @@ bool TokenStream::incrementToken() {
     const auto& term = terms_[pos_];
     //offsetAtt->setOffset(correctOffset(0), finalOffset);
     term_att_->setTermLength(term.length());
-    term_att_->resizeTermBuffer(term.length() + 1);
-    auto trgt = term_att_->termBuffer();
-    memcpy(trgt.get(), term.data(), trgt.size());
+    term_att_->setTermBuffer(term);
     return true;
   }
 }
 
 void TokenStream::addToken(const fnord::String token) {
-  terms_.emplace_back(token);
+  terms_.emplace_back(StringUtil::convertUTF8To16(token));
 }
 
 }
