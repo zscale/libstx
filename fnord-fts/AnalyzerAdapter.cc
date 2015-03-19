2caa3d17c219d46b97aff3e12b68a20a4ffaab95
Paul Asmuth <paul@paulasmuth.com>
2015-03-19 21:17:39 +0100

    StringUtil::convertUTF{8,16}To{8,16}

--- fnord-fts/AnalyzerAdapter.cc
+++ fnord-fts/AnalyzerAdapter.cc
@@ -8,7 +8,7 @@
 * <http://www.gnu.org/licenses/>.
 */
 #include "fnord-base/inspect.h"
-#include "fnord-base/utf8.h"
+#include "fnord-base/UTF8.h"
 #include "fnord-fts/fts.h"
 #include "fnord-fts/fts_common.h"
 #include "fnord-fts/AnalyzerAdapter.h"
@@ -17,15 +17,16 @@ namespace fnord {
 namespace fts {
 
 TokenStreamPtr AnalyzerAdapter::tokenStream(
-    const String& field_name,
+    const String& field_name16,
     const ReaderPtr& reader) {
+  std::string field_name = StringUtil::convertUTF16To8(field_name16);
   std::string field_str;
   wchar_t chr;
   while ((chr = reader->read()) > 0) {
     UTF8::encodeCodepoint(chr, &field_str);
   }
 
-  fnord::iputs("tokenize: $0 => ($2)", field_name, field_str);
+  fnord::iputs("tokenize: $0 => $1", field_name, field_str);
   return std::make_shared<TokenStreamAdapter>();
 }
 
