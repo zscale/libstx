fe899882fac4ff674367b7aa6d5b21d76dc87b06
Christian Parpart <trapni@gmail.com>
2015-03-20 23:32:14 +0100

    s/GPLv3/AGPLv3/

--- xzero-base/StringUtil.h
+++ xzero-base/StringUtil.h
@@ -3,11 +3,10 @@
 //   (c) 2014-2015 Paul Asmuth <https://github.com/paulasmuth>
 //
 // libxzero is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License v3.0. You should have received a
-// copy of the GNU General Public License along with this program. If not, see
-// <http://www.gnu.org/licenses/>.
+// the terms of the GNU Affero General Public License v3.0.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-// Copyright (c) 2014 Paul Asmuth
 #pragma once
 
 #include <stdlib.h>
