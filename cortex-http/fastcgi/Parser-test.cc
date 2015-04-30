e76b769126d3f0c33e83170716acbeff4eb0c777
Christian Parpart <trapni@gmail.com>
2015-04-30 22:59:29 +0200

    [http] more work on fastcgi (parser)

--- cortex-http/fastcgi/Parser-test.cc
+++ cortex-http/fastcgi/Parser-test.cc
@@ -6,27 +6,110 @@
 #include <cortex-http/HttpRequest.h>
 #include <cortex-http/HttpResponse.h>
 #include <cortex-http/HttpChannel.h>
-#include <cortex-base/executor/DirectExecutor.h>
 
 using namespace cortex;
 
+class TestListener : public HttpListener { // {{{
+ public:
+  bool onMessageBegin(const BufferRef& method, const BufferRef& entity,
+                      HttpVersion version) override;
+  bool onMessageBegin(HttpVersion version, HttpStatus code,
+                      const BufferRef& text) override;
+  bool onMessageBegin() override;
+  bool onMessageHeader(const BufferRef& name, const BufferRef& value) override;
+  bool onMessageHeaderEnd() override;
+  bool onMessageContent(const BufferRef& chunk) override;
+  bool onMessageEnd() override;
+  void onProtocolError(HttpStatus code, const std::string& message) override;
+
+ public:
+  BufferRef method;
+  BufferRef entity;
+  HttpVersion version = HttpVersion::VERSION_0_9;
+  HttpStatus status = HttpStatus::Undefined;
+  BufferRef text;
+  int requestMessageBeginCount = 0;
+  int genericMessageBeginCount = 0;
+  int responseMessageBeginCount = 0;
+  HeaderFieldList headers;
+  int headersEnd = 0;
+  int messageEnd = 0;
+  Buffer body;
+};
+
+bool TestListener::onMessageBegin(const BufferRef& method, const BufferRef& entity,
+                    HttpVersion version) {
+  this->method = method;
+  this->entity = entity;
+  this->version = version;
+  this->requestMessageBeginCount++;
+
+  return true;
+}
+
+bool TestListener::onMessageBegin(HttpVersion version, HttpStatus code,
+                    const BufferRef& text) {
+  this->version = version;
+  this->status = code;
+  this->text = text;
+  this->responseMessageBeginCount++;
+
+  return true;
+}
+
+bool TestListener::onMessageBegin() {
+  this->genericMessageBeginCount++;
+
+  return true;
+}
+
+bool TestListener::onMessageHeader(const BufferRef& name, const BufferRef& value) {
+  printf("TestListener.onMessageHeader(%*s: %*s)\n",
+      (int) name.size(), name.data(),
+      (int) value.size(), value.data());
+  this->headers.push_back(name.str(), value.str());
+  return true;
+}
+
+bool TestListener::onMessageHeaderEnd() {
+  printf("TestListener.onMessageHeaderEnd\n");
+  this->headersEnd++;
+  return true;
+}
+
+bool TestListener::onMessageContent(const BufferRef& chunk) {
+  printf("TestListener.onMessageContent: %zu\n", chunk.size());
+  this->body.push_back(chunk);
+  return true;
+}
+
+bool TestListener::onMessageEnd() {
+  this->messageEnd++;
+  return true;
+}
+
+void TestListener::onProtocolError(HttpStatus code, const std::string& message) {
+}
+// }}}
+
 Buffer makeParams(const std::list<std::pair<std::string, std::string>>& params) {
-  Buffer result;
-
   http::fastcgi::CgiParamStreamWriter w;
-  for (const auto param: params) {
-    printf("encoding param <%s, %s>\n", param.first.c_str(), param.second.c_str());
+  for (const auto param: params)
     w.encode(param.first, param.second);
-  }
-  printf("param stream size: %zu\n", w.output().size());
 
-  result.reserve(sizeof(http::fastcgi::Record) + w.output().size());
-  printf("param record size: %zu\n", result.capacity());
+  Buffer result;
+  result.reserve(2 * sizeof(http::fastcgi::Record) + w.output().size());
 
+  // append Params record
   new(result.data()) http::fastcgi::Record(
       http::fastcgi::Type::Params, 42, w.output().size(), 0);
   result.resize(sizeof(http::fastcgi::Record));
   result.push_back(w.output());
+
+  // append Params-EOS
+  new(result.data() + result.size()) http::fastcgi::Record(
+      http::fastcgi::Type::Params, 42, 0, 0);
+  result.resize(result.size() + sizeof(http::fastcgi::Record));
 
   return result;
 }
@@ -42,25 +125,18 @@ TEST(http_fastcgi_Parser, simpleRequest) {
   // {FCGI_STDIN,           1, ""}
 
   int parsedRequestId = -1;
-  Buffer parsedContent;
   std::vector<std::pair<std::string, std::string>> parsedHeaders;
 
-  DirectExecutor executor;
-  auto handler = [&](cortex::HttpRequest* req, cortex::HttpResponse* resp) { };
+  TestListener httpListener;
   auto onCreateChannel = [&](int requestId) -> HttpListener* {
-    parsedRequestId = requestId;
-    return new HttpChannel(
-        nullptr, // transport
-        handler,
-        new MockInput(),
-        1024, // maxRequestUriLength,
-        1024, // maxRequestBodyLength
-        nullptr); // outputCompressor
-  };
+                            parsedRequestId = requestId;
+                            return &httpListener; };
   auto onUnknownPacket = [&](int requestId, int recordId) { };
   auto onAbortRequest = [&](int requestId) { };
+  auto onStdErr = [&](int requestId, const BufferRef& content) { };
 
-  http::fastcgi::Parser p(onCreateChannel, onUnknownPacket, onAbortRequest);
+  http::fastcgi::Parser p(onCreateChannel, onUnknownPacket, onAbortRequest,
+                          onStdErr);
 
   size_t n = p.parseFragment<http::fastcgi::BeginRequestRecord>(
       http::fastcgi::Role::Responder, 42, false);
@@ -72,6 +148,7 @@ TEST(http_fastcgi_Parser, simpleRequest) {
       {"SERVER_PROTOCOL", "1.1"},
       {"SERVER_NAME", "www.example.com"},
       {"HTTP_USER_AGENT", "cortex-test"},
+      {"HTTP_CONTENT_TYPE", "text/plain"},
   });
   n = p.parseFragment(params);
   printf("parsed params bytes: %zu\n", n);
@@ -87,7 +164,7 @@ TEST(http_fastcgi_Parser, simpleRequest) {
   n = p.parseFragment(pdata);
 
   ASSERT_EQ(pdata.size(), n);
-  ASSERT_EQ(content, parsedContent);
+  ASSERT_EQ(content, httpListener.body);
 
   // EOS
   new(data) http::fastcgi::Record(
